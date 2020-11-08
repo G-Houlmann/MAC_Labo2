4cdb9ec7700821ea769cb78b0dd7871b287ac96b
Gildas <gildas.houlmann@gmail.com>
2020-11-08 17:32:17 +0100

    fix r-precision

--- src/main/java/ch/heigvd/iict/mac/labo2/Evaluation.java
+++ src/main/java/ch/heigvd/iict/mac/labo2/Evaluation.java
@@ -214,7 +214,8 @@ public class Evaluation {
             double recall, RPrecision;
             if(relevantDocs != 0){
                 recall = (double) retrievedRelevantDocs / relevantDocs;
-                RPrecision = (double) retrievedRelevantDocs / relevantDocs;
+                long amoutRelevantRetrievedinXFirst = queryResults.stream().limit(relevantDocs).filter(qrelResults::contains).count();
+                RPrecision = (double) amoutRelevantRetrievedinXFirst / relevantDocs;
             }else{
                 recall = RPrecision = 0;
             }
@@ -225,10 +226,10 @@ public class Evaluation {
             avgPrecision += (precision - avgPrecision) / queryNumber;
             avgRecall += (recall - avgRecall) / queryNumber;
             avgRPrecision += (RPrecision - avgRPrecision) / queryNumber;
+            //avgRPrecision += RPrecision;
             meanAveragePrecision += (AP - meanAveragePrecision) / queryNumber;
 
         }
-
 
         fMeasure = (avgRecall + avgPrecision == 0)? 0 : (2 * avgRecall * avgPrecision) / (avgRecall + avgPrecision);
 
